4aaeea3bddf869ee3ed548c94b64f6607cca0467
Lucas Hoffmann <loegcas@gmail.com>
2026-02-16 20:06:00 +0100

    fixed some bugs again

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const hamburgerBtn = document.querySelector(".hamburger-menu-container");
-const hamburgerIcon = document.querySelector("#hamburger-menu")
-const close = document.querySelector("#close");
+const hamburgerIcon = document.querySelector("#hamburger-menu");
+const closeIcon = document.querySelector("#close");
 const menu = document.querySelector(".menu");
-const backdrop = document.querySelector(".backdrop");
-const backgroundVideo = document.querySelector(".background-video");
 
 hamburgerBtn.addEventListener("click", () => {
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-        hamburgerIcon.style.display = "flex"
-        close.style.display = "none"
+    menu.classList.toggle("active");
+
+    if (menu.classList.contains("active")) {
+        hamburgerIcon.style.display = "none";
+        closeIcon.style.display = "block";
     } else {
-        menu.style.display = 'flex';
-        hamburgerIcon.style.display = "none"
-        close.style.display = "flex"
+        hamburgerIcon.style.display = "block";
+        closeIcon.style.display = "none";
     }
 });
-backgroundVideo.addEventListener("click", () => {
-    if (menu.style.display === "flex") {
-        menu.style.display = 'none';
-        hamburgerIcon.style.display = "flex"
-        close.style.display = "none"
-    }
-})
